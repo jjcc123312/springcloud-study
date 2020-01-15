cf8261393820f6d89a409ca113423b47c0e158b1
jjcc123312 <1542123803@qq.com>
2020-01-15 15:21:33 +0800

    config-client更新配置文件，通过POST请求localhost:port/actuator/refresh会更新bootstart.yml文件。

--- config-client/src/main/java/com/config/controller/HelloController.java
+++ config-client/src/main/java/com/config/controller/HelloController.java
@@ -1,17 +1,20 @@
 package com.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 /**
+ * @RefreshScope使用该类的注解，会在接到SpringCloud配置中心的配置更新时，自动将新的配置更新到该类对应的字段中
  * @author Jjcc
  * @version 1.0.0
  * @className HelloController.java
  * @createTime 2020年01月15日 10:22:00
  */
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Value("${info.profile:error}")
